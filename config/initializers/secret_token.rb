6fc3ba07a844f6da072bb5a7d88805706e751b8a
h0rm0n <hormon2343@gmail.com>
2011-09-29 11:39:31 +0200

    Lista1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lista1::Application.config.secret_token = '0508480e651ebe668757ee16e065136581df54ea98dc25a55b2ade0496d964d5d250fcaef10386ebdde4aef27712e9098f20368495cf294d5db19815a281290c'
